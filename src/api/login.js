8b9f8ce2d1276edd27aa6c532aa99325ef9b71c6
wangxun <945097967@qq.com>
2021-05-17 17:52:27 +0800

    前端登录

---
+++ src/api/login.js
@@ -0,0 +1,24 @@
+import request from '@/utils/request'
+
+export function login(userInfo) {
+  return request({
+    url: '/pub/login',
+    method: 'post',
+    data: userInfo
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/user/userInfo',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/pub/logout',
+    method: 'post'
+  })
+}
